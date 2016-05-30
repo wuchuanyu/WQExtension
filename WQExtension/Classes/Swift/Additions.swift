ef790a6618cce8fdcb728f8618f30468f371caca
wuchuanyu <wu.chuanyu@163.com>
2016-05-30 17:27:00 +0800

    +swift additions

---
+++ WQExtension/Classes/Swift/Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Additions.swift
+//  WQExtension
+//
+//  Created by wuchuanyu on 16/5/30.
+//  Copyright © 2016年 wuchuanyu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let gScreenWidth = UIScreen.mainScreen().bounds.size.width
+let gScreenHeight = UIScreen.mainScreen().bounds.size.height
